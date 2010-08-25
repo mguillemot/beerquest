1965473aed2dad1a89c1773521f399da77cb2533
Matthieu Guillemot <g@npng.org>
2010-08-25 23:03:29 +0900

    fk

--- www/app/models/barship.rb
+++ www/app/models/barship.rb
@@ -1,8 +1,8 @@
 class Barship
   include DataMapper::Resource
 
-  property :account_id, Integer, :key => true
-  property :bar_id, Integer, :key => true
+  property :account_id, Integer, :min => 1, :key => true # Note 1: these 2 properties are necessary besides the belongs_to declarations to declare the composite key
+  property :bar_id, Integer, :min => 1, :key => true     # Note 2: :min => 1 is necessary to have UNSIGNED INT to reference the column as FK
   property :play_count, Integer
   property :last_play, DateTime
   property :total_beers, Integer
