9e1b51dfa43dcb006276280965c2a7e8b91b7a68
Matthieu Guillemot <g@npng.org>
2010-08-25 23:08:20 +0900

    fk2

--- www/app/models/friendship.rb
+++ www/app/models/friendship.rb
@@ -1,8 +1,8 @@
 class Friendship
   include DataMapper::Resource
 
-  property :account_id, Integer, :key => true
-  property :friend_id, Integer, :key => true
+  property :account_id, Integer, :min => 1, :key => true # Note 1: these 2 properties are necessary besides the belongs_to declarations to declare the composite key
+  property :friend_id, Integer, :min => 1, :key => true  # Note 2: :min => 1 is necessary to have UNSIGNED INT to reference the column as FK
   property :created_at, DateTime
   property :updated_at, DateTime
 
