a5405275432f8a0f58dc412746092ecfea60781d
Matthieu Guillemot <erhune@erhunix.(none)>
2010-10-17 01:00:51 +0900

    challenged en home

--- www/app/controllers/user_controller.rb
+++ www/app/controllers/user_controller.rb
@@ -1,6 +1,7 @@
 class UserController < FacebookController
 
   BARS_PER_PAGE = 3
+  CHALLENGES_PER_PAGE = 8
   WORLD_SCORE_TARGETS = [
           {:score => 1_000, :challenge => 'challenge.value.target1000'},
           {:score => 2_000, :challenge => 'challenge.value.target2000'},
@@ -8,13 +9,67 @@ class UserController < FacebookController
   ]
 
   def index
-    async_world_score
-    async_favorites
-    async_partners
-    async_search
+    set_world_score
+    set_favorites(1)
+    set_partners(1)
+    set_search('(none)', 1)
+    set_current_challenges(1)
+    set_received_challenges(1)
+    set_sent_challenges(1)
   end
 
   def async_world_score
+    set_world_score
+    respond_to do |format|
+      format.js { render :layout => false }
+    end
+  end
+
+  def async_favorites
+    set_favorites(params[:page].to_i)
+    respond_to do |format|
+      format.js { render :layout => false }
+    end
+  end
+
+  def async_partners
+    set_partners(params[:page].to_i)
+    respond_to do |format|
+      format.js { render :layout => false }
+    end
+  end
+
+  def async_search
+    set_search(params[:prefix], params[:page].to_i)
+    respond_to do |format|
+      format.js { render :layout => false }
+    end
+  end
+
+  def async_current_challenges
+    set_current_challenges(params[:page].to_i)
+    respond_to do |format|
+      format.js { render :layout => false }
+    end
+  end
+
+  def async_received_challenges
+    set_received_challenges(params[:page].to_i)
+    respond_to do |format|
+      format.js { render :layout => false }
+    end
+  end
+
+  def async_sent_challenges
+    set_sent_challenges(params[:page].to_i)
+    respond_to do |format|
+      format.js { render :layout => false }
+    end
+  end
+
+  protected
+
+  def set_world_score
     @world_score = World.total_beers
     @world_score_increase = World.increase_last_hour / 3600.0
     target = WORLD_SCORE_TARGETS.find { |t| t[:score] > @world_score }
@@ -27,24 +82,24 @@ class UserController < FacebookController
     end
   end
 
-  def async_favorites
+  def set_favorites(page)
     results = @me.favorite_bars
-    @favorites_page = (params[:page] || 1).to_i
-    @favorites_max_page = (results.count.to_f / BARS_PER_PAGE).ceil
+    @favorites_page = page
+    @favorites_max_page = [1, (results.count.to_f / BARS_PER_PAGE).ceil].max
     @favorites_total = results.count
     @favorites = results.all(:limit => BARS_PER_PAGE, :offset => (@favorites_page - 1) * BARS_PER_PAGE)
   end
 
-  def async_partners
+  def set_partners(page)
     results = Bar.all
-    @partners_page = (params[:page] || 1).to_i
-    @partners_max_page = (results.count.to_f / BARS_PER_PAGE).ceil
+    @partners_page = page
+    @partners_max_page = [1, (results.count.to_f / BARS_PER_PAGE).ceil].max
     @partners_total = results.count
     @partners = results.all(:limit => BARS_PER_PAGE, :offset => (@partners_page - 1) * BARS_PER_PAGE)
   end
 
-  def async_search
-    @search_prefix = params[:prefix] || '(none)'
+  def set_search(prefix, page)
+    @search_prefix = prefix
     if @search_prefix == '123'
       results = Bar.all(:name.like => "1%") +
               Bar.all(:name.like => "2%") +
@@ -61,15 +116,37 @@ class UserController < FacebookController
     end
 
     @search_max_page = [1, (results.count.to_f / BARS_PER_PAGE).ceil].max
-    if params[:page]
-      @search_page = params[:page].to_i
-    end
+    @search_page = page
     if !@search_page || @search_page < 1 || @search_page > @search_max_page
       @search_page = 1
     end
 
     @search_total = results.count
     @search = results.all(:limit => BARS_PER_PAGE, :offset => (@search_page - 1) * BARS_PER_PAGE)
+  end
+
+  def set_current_challenges(page)
+    results = Battle.all
+    @current_challenges_page = page
+    @current_challenges_max_page = [1, (results.count.to_f / CHALLENGES_PER_PAGE).ceil].max
+    @current_challenges_total = results.count
+    @current_challenges = results.all(:limit => CHALLENGES_PER_PAGE, :offset => (@current_challenges_page - 1) * CHALLENGES_PER_PAGE)
+  end
+
+  def set_received_challenges(page)
+    results = Battle.all
+    @received_challenges_page = page
+    @received_challenges_max_page = [1, (results.count.to_f / CHALLENGES_PER_PAGE).ceil].max
+    @received_challenges_total = results.count
+    @received_challenges = results.all(:limit => CHALLENGES_PER_PAGE, :offset => (@received_challenges_page - 1) * CHALLENGES_PER_PAGE)
+  end
+
+  def set_sent_challenges(page)
+    results = Battle.all
+    @sent_challenges_page = page
+    @sent_challenges_max_page = [1, (results.count.to_f / CHALLENGES_PER_PAGE).ceil].max
+    @sent_challenges_total = results.count
+    @sent_challenges = results.all(:limit => CHALLENGES_PER_PAGE, :offset => (@sent_challenges_page - 1) * CHALLENGES_PER_PAGE)
   end
 
 end
--- www/config/routes.rb
+++ www/config/routes.rb
@@ -1,30 +1,33 @@
 BeerQuest::Application.routes.draw do
 
   # User pages
-  root                         :to => "user#index",             :as => 'home'
-  get "world-score",          :to => "user#async_world_score", :as => 'async_world_score'
-  get "favorites/:page",      :to => "user#async_favorites",   :as => 'async_favorites'
-  get "partners/:page",       :to => "user#async_partners",    :as => 'async_partners'
-  get "search/:prefix/:page", :to => "user#async_search",      :as => 'async_search'
+  root                              :to => "user#index",                     :as => 'home'
+  get "world-score",               :to => "user#async_world_score",         :as => 'async_world_score'
+  get "bars/favorites/:page",      :to => "user#async_favorites",           :as => 'async_favorites'
+  get "bars/partners/:page",       :to => "user#async_partners",            :as => 'async_partners'
+  get "bars/search/:prefix/:page", :to => "user#async_search",              :as => 'async_search'
+  get "challenges/current/:page",  :to => "user#async_current_challenges",  :as => 'async_current_challenges'
+  get "challenges/sent/:page",     :to => "user#async_current_challenges",  :as => 'async_sent_challenges'
+  get "challenges/received/:page", :to => "user#async_received_challenges", :as => 'async_received_challenges'
 
   # Bar pages
-  get "bar/:id",              :to => "bar#index",              :as => 'bar'
+  get "bar/:id",                   :to => "bar#index",                      :as => 'bar'
 
   # Static pages
-  get "support_us",           :to => "static#support_us",      :as => 'support_us'
-  get "privacy",              :to => "static#privacy",         :as => 'privacy'
-  get "tos",                  :to => "static#tos",             :as => 'tos'
+  get "support_us",                :to => "static#support_us",              :as => 'support_us'
+  get "privacy",                   :to => "static#privacy",                 :as => 'privacy'
+  get "tos",                       :to => "static#tos",                     :as => 'tos'
 
   # Facebook integration
-  get "session_login",       :to => "facebook#session_login",  :as => 'login'
-  get "session_logout",      :to => "facebook#session_logout", :as => 'logout'
+  get "session_login",            :to => "facebook#session_login",          :as => 'login'
+  get "session_logout",           :to => "facebook#session_logout",         :as => 'logout'
 
   # Game integration
-  get "start",               :to => "game#start",              :as => 'game_start'
-  post "postscore",          :to => "game#postscore",          :as => 'post_score'
+  get "start",                    :to => "game#start",                      :as => 'game_start'
+  post "postscore",               :to => "game#postscore",                  :as => 'post_score'
 
   # Admin & debug
-  get "play/:id" ,           :to => "admin#hack_login"
-  get "debug/:id",           :to => "admin#check_game"
+  get "play/:id" ,                :to => "admin#hack_login"
+  get "debug/:id",                :to => "admin#check_game"
 
 end
