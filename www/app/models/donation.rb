846622e762df6a4d24f2c1408e80be4a5c7b3272
Matthieu Guillemot <erhune@erhunix.(none)>
2010-10-17 20:58:00 +0900

    intégration paypal

--- www/app/models/donation.rb
+++ www/app/models/donation.rb
@@ -1,12 +1,21 @@
 class Donation
   include DataMapper::Resource
 
+  DONATIONS = {
+          1 => {:amount => 0.42, :name => 'support_us.offers.offer1.name'},
+          2 => {:amount => 1.5, :name => 'support_us.offers.offer2.name'},
+          3 => {:amount => 3, :name => 'support_us.offers.offer3.name'},
+          4 => {:amount => 5, :name => 'support_us.offers.offer4.name'},
+          5 => {:amount => 10, :name => 'support_us.offers.offer5.name'},
+          6 => {:amount => 100, :name => 'support_us.offers.offer6.name'}
+  }
+
   property :id, Serial
   property :account_id, Integer, :min => 1
   property :level, Integer
   property :paypal_email, String
   property :paypal_name, String
-  property :paypal_amount, Decimal
+  property :paypal_amount, Decimal, :precision => 20, :scale => 10
   property :paypal_currency, String
   property :paypal_payer_id, String
   property :paypal_correlation_id, String
@@ -16,12 +25,25 @@ class Donation
 
   belongs_to :account
 
+  def self.total
+    self.all().sum(:paypal_amount)
+  end
+
+  def amount
+    DONATIONS[level][:amount]
+  end
+
+  def currency
+    'EUR'
+  end
+
   def name
-    "Grosse chope"
+    key = DONATIONS[level][:name]
+    t(key)
   end
 
   def description
-    "Donation BeerQuest"
+    t('support_us.donation')
   end
 
 end
