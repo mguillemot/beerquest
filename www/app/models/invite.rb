f6ca0844ced5386ac91b34bd018cc6ca9580ba25
Matthieu Guillemot <g@npng.org>
2011-01-30 23:29:08 +0900

    plus d'infos sur les invites

--- www/app/models/invite.rb
+++ www/app/models/invite.rb
@@ -8,7 +8,7 @@ class Invite
   property :data, String, :length => 0..255
   property :message, String, :length => 0..255
   property :friend_facebook_id, Integer, :min => 0, :max => 2**64-1, :required => true, :index => true
-  property :friend_facebook_name, String, :length => 0..255
+  property :friend_name, String, :length => 0..255
   property :accepted_by, Integer, :min => 1 # TODO ajouter l'association
   property :accepted_at, DateTime
   property :created_at, DateTime
