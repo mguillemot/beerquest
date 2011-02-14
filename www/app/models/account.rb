89a5885a4f50547eaca66d6d433ea15d2f265c5f
Erhune <g@npng.org>
2011-02-15 00:17:00 +0900

    dernier bar joué

--- www/app/models/account.rb
+++ www/app/models/account.rb
@@ -33,9 +33,8 @@ class Account
   end
 
   def last_bar
-    # TODO updater les barships pour savoir lequel est le VRAI last
-    last_barship = barships.first(:order => :updated_at.desc)
-    last_barship ? last_barship.bar : Bar.default_bar
+    last_replay = replays.first(:order => :created_at.desc)
+    last_replay ? replay.bar : Bar.default_bar
   end
 
   def total_beers
