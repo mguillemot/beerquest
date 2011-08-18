cbf7d74f372ab0f2e4206b9321215d40caf1af75
Matthieu Guillemot <mguillemot@npng.org>
2011-08-18 19:37:09 +0900

    Fix hosting settings (Unicorn)

--- www/unicorn.rb
+++ www/unicorn.rb
@@ -1,13 +1,12 @@
-APP_PATH = "/home/www/www.bq-4.com/www"
+APP_PATH = "/home/ruby/www.bq-4.com/www"
 LOG_PATH = "/var/log/unicorn"
 SYS_PATH = "/var/run/unicorn"
 
-
-worker_processes 4
+worker_processes  1 # Because there isn't much power on this box...
 working_directory APP_PATH
-listen "#{SYS_PATH}/beerquest.socket", :backlog => 64
-timeout 30
-user "www-data"
-pid "#{SYS_PATH}/beerquest.pid"
-stderr_path "#{LOG_PATH}/beerquest.stderr.log"
-stdout_path "#{LOG_PATH}/beerquest.stdout.log"
+listen            "#{SYS_PATH}/beerquest.socket", :backlog => 64
+timeout           30
+user              "www-data"
+pid               "#{SYS_PATH}/beerquest.pid"
+stderr_path       "#{LOG_PATH}/beerquest.stderr.log"
+stdout_path       "#{LOG_PATH}/beerquest.stdout.log"
